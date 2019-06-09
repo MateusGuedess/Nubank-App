915873800c9c9eebb1c47d1aba8416438cce6e8e
Mateus Guedes <teuguedes@outlook.com>
2019-06-09 02:17:23 -0300

    end

--- .history/src/components/Header/styles_20190608140817.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-  align-items: center;
-  padding: 40px 0 30px;
-`;
--- .history/src/pages/Main/styles_20190608140327.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-import { View } from 'react-native';
-
-export const Container = styled.View`
-  flex: 1;
-  background: #8B10AE;
-`;
